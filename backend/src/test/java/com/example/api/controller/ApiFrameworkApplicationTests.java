8151660200ddc83a36e59a3a25c54d0d382c71d8
Victor Manoilescu <Victor.Manoilescu@endava.com>
2026-05-13 12:36:21 +0300

    fixed typo in code

--- backend/src/test/java/com/example/api/controller/ApiFrameworkApplicationTests.java
+++ backend/src/test/java/com/example/api/controller/ApiFrameworkApplicationTests.java
@@ -39,7 +39,7 @@ class ApiFrameworkApplicationTests {
    /* @Test
     void protectedEndpointRequiresAuthentication() throws Exception {
         mockMvc.perform(get("/api/transactions"))
-                .andExpect(status().isForbidden());*/
-    }
+                .andExpect(status().isForbidden());
+    }*/
 
 }
